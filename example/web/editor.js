2ec8b39ad40cd737682c5559f9cb058f12ad9e3d
Brian Mock <mock.brian@gmail.com>
2014-02-17 23:24:37 -0800

    Small tweaks

--- example/web/editor.js
+++ example/web/editor.js
@@ -3,10 +3,24 @@
     var session = editor.getSession();
 
     // List of themes: https://github.com/ajaxorg/ace/tree/master/lib/ace/theme
-    editor.setTheme("ace/theme/eclipse");
+    editor.setTheme("ace/theme/github");
     editor.setKeyboardHandler("ace/keyboard/vim");
     session.setTabSize(4);
     session.setUseSoftTabs(true);
+
+    editor.setValue("");
+    editor.insert([
+        "twice (",
+        "    twice (",
+        "        count.",
+        "        count.",
+        "    ).",
+        "    count.",
+        ").",
+        "twice (",
+        "    count.",
+        ").",
+    ].join("\n"));
 
     GLOBAL.editor = editor;
 })(this);
